32b641d6345a8a7371338a0c057a8de16309ea5c
JakubKret <jakub.kret04@gmail.com>
2026-03-10 15:59:14 +0100

    Haven't done much but need to go, that's where I left it

---
+++ assets/scripts/app.js
@@ -0,0 +1,5 @@
+//import { apiKey } from "./util";
+//import apiKey from "./util";
+//import { apiKey, abc as content } from "./util";
+import * as util from "./util";
+console.log(util.apiKey);
---
+++ assets/scripts/util.js
@@ -0,0 +1,4 @@
+//export let apiKey = "placeholder text";
+export default "placeholder text";
+export let apiKey = "placeholder text2";
+export let abc = "abc";
